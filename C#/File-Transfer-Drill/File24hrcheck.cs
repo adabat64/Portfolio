9af024d829d2512b937062365cb9a3e27c290b0e
adabat64 <adrian.dabat@gmail.com>
2016-09-04 17:33:37 -0700

    Added transfer file CS program

---
+++ C#/File-Transfer-Drill/File24hrcheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCheckApp
+{
+    class FileCheck
+    {
+        public string src = @"C:\Users\Owner\Desktop\Source\";
+        // string file = System.IO.Path.Combine(src, filename);
+
+        public void processDirectory()
+        {
+            string[] fileEntries = Directory.GetFiles(src);
+            foreach (string filename in fileEntries)
+                ProcessFile(filename);
+        }
+        public void ProcessFile(string filename)
+        {
+            Console.WriteLine(filename);
+            DateTime lastModified = System.IO.File.GetLastWriteTime(filename);
+            //Console.WriteLine(lastModified);
+            DateTime check = DateTime.Now.AddHours(-24);
+           // Console.WriteLine(check);
+            string temp = filename.Remove(0, 30);
+            Console.WriteLine(temp);
+            string dest = "C:\\Users\\Owner\\Desktop\\Dest\\" + temp;
+            //Console.WriteLine(dest);
+
+
+            if (lastModified >= check) {
+                Console.WriteLine(lastModified - check);
+                File.Move(filename,dest);
+            }
+        }
+    }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                FileCheck fc = new FileCheck();
+                fc.processDirectory();
+
+
+            }
+        }
+    }
---
+++ C#/File-Transfer-Drill/README.md
@@ -0,0 +1,16 @@
+### File Transfer Drill
+***
+
+This small file transfer program I created checks a source folder for files that were last modified after t-24hrs, and automatically moves them to a destination folder.
+For example you execute this program on Monday at 3pm, it will check for files that have a last modified date stamp between Sunday at 3pm and Monday at 3pm.
+
+##### Instructions
+WINDOWS ONLY
+
+* Download the File24hrcheck.cs, and open it.
+* In the "FileCheck" class, change the "src" string with your source folder location,
+* In the "processFile" method, change the "dest" string with your destination folder location.
+
+* Compile and execute program.
+
+Return to [portfolio](../../../../)
